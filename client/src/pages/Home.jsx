9b93c4ce0c3d5ef26ec9c4e376a4eeb6ff354590
teleidoskopbild <teleidoskopbild@gmail.com>
2024-11-25 11:58:16 +0100

    add board layout selection and basic create board page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Welcome to Shareboard!</h1>
+      <div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/selectboard">
+          <button>Create a Board</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
