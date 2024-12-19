cdedce77f41077c25c83ae69d6b8dd007219996a
teleidoskopbild <teleidoskopbild@gmail.com>
2024-12-19 12:04:44 +0100

    add ssl support

--- server/src/util/db-connect.js
+++ server/src/util/db-connect.js
@@ -11,6 +11,9 @@ const db = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: process.env.POSTGRES_SSL === "true" && {
+      rejectUnauthorized: false,
+    },
   },
 });
 
